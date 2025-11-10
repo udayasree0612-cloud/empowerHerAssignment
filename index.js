d683e38fdbb0583682287449792549a26c127497
udayasree0612-cloud <udayasree0612@gmail.com>
2025-11-10 21:49:40 +0530

    this is intial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+console.log("Hello, World!");
+
+
+alert("Welcome to JavaScript!");
+
+// Do a simple math operation
+let a = 5;
+let b = 3;
+let sum = a + b;
+
+console.log("The sum of a and b is: " + sum);
